ec272476b281874c26151e3900b9944b8ba6b9d0
Leon Yip <leonyipwh@hotmail.com>
2015-12-08 00:35:42 +0800

    post method and instruction guide

--- http/http.js
+++ http/http.js
@@ -13,7 +13,21 @@ if (Meteor.isClient) {
           console.log("error", error);
         }
         else {
-          console.log(result.data.hits.hits);
+          var result = result.data.hits.hits;
+          console.log(result);
+          $('#result').html(result);
+        }
+      });
+    },
+    'click #post': function () {
+      var query = {name:'omg',details:'421'};
+
+      Meteor.call("post", query, function(error, result) {
+        if(error){
+          console.log("error", error);
+        }
+        else {
+          console.log(result);
         }
       });
     }
--- http/server/methods.js
+++ http/server/methods.js
@@ -1,11 +1,18 @@
 if (Meteor.isServer) {
   Meteor.methods({
-    get:function(query){
-      this.unblock();
+    get:function(query) {
       var result = HTTP.call("GET", 'https://aws-us-east-1-portal9.dblayer.com:10159/yesmaster/_search?q='+ query,
       {auth:'admin:19degrees'});
       console.log(result);
       return result;
+    },
+    post:function(query) {
+      var result = HTTP.call("PUT", 'https://aws-us-east-1-portal9.dblayer.com:10159/yesmaster/products/2',
+      {
+        auth:'admin:19degrees',
+        data: query
+      });
+      console.log(result);
     }
   });
 }
