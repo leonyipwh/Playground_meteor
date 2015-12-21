9d01be3ad8700f373fcb4d0b64b90576687ea3e9
Leon Yip <leonyip@bbi.io>
2015-12-21 19:13:03 +0800

    react playground

---
+++ react/app.jsx
@@ -0,0 +1,54 @@
+// App component - represents the whole app
+App = React.createClass({
+  getTasks() {
+    return [
+      { _id: 1, text: "This is task 1" },
+      { _id: 2, text: "This is task 2" },
+      { _id: 3, text: "This is task 3" }
+    ];
+  },
+
+  renderTasks() {
+    return this.getTasks().map((task) => {
+      return <Task key={task._id} task={task} />;
+    });
+  },
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1>Todo List</h1>
+        </header>
+
+        <ul>
+          {this.renderTasks()}
+        </ul>
+
+        <Input />
+        <div>value</div>
+      </div>
+    );
+  }
+});
+
+Input = React.createClass({
+  getInput(event) {
+    var data = event.target.value;
+    console.log(data);
+  },
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Input" onChange={this.getInput}/>
+      </div>
+    );
+  }
+});
+
+// Testing = React.createClass({
+//   render() {
+//     return (
+//       <div>hi</div>
+//     );
+//   }
+// });
